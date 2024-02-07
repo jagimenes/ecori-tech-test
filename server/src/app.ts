03135b43e3fe7506162a781882e6c09dd5709f15
joaopaulosf <jpforcode@gmail.com>
2024-02-07 17:12:56 -0300

    refactor: update cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ const upload = multer({ dest: "uploads/" });
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: ["http://127.0.0.1:5173", "http://127.0.0.1:4173"],
   })
 );
 
